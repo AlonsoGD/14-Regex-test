c28fa1c39e9560012d77d3d6d6d192fc5a46aa17
AlonsoGD <dani.alonso.garcia@gmail.com>
2018-01-25 17:00:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  var regexCode;
-
+  var result;
+  
   function addText(str) {
     document.getElementById("showTextBox").innerText = str;
   } 
+
   function getText() {
     return document.getElementById("userText").value;
   }
-  function getRegexCode() {
-    return document.getElementById("codeInput").value;;
+
+  // function getRegexCode() {
+  //   return document.getElementById("codeInput").value;
+  // }
+
+  function createReplacerObj() {
+    var replacer;
+    var originalValue = document.getElementById("original_1").value;
+    var replacerValue = document.getElementById("replacer_1").value;
+    return replacer = {
+      [originalValue] : replacerValue,
+    }
   }
 
-  function applyRegex() {
-    var re = new RegExp(regexCode, "i")
-    var result = getText().match(re);
-    console.log(result);
+  function applyRegex(str) {
+    var re = new RegExp(document.getElementById("original_1").value, "g");
+    var str = str.replace(re, function (match) {
+      return createReplacerObj()[match];
+    })
+    result = str;
+  }
+
+  function showResult () {
+    var resultBox = document.getElementById("resultBox");
+    var resultText = document.getElementById("resultText");
+    resultText.classList.remove("hidden");
+    resultBox.innerText = result;
   }
 
   function main() {
@@ -26,8 +46,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 
     myForm.addEventListener("submit", function (event) {
-      regexCode = getRegexCode();
-      applyRegex();
+      applyRegex(getText());
+      showResult();
       event.preventDefault();
     });
   };
